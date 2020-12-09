fe16ab4bf1cc1c5cebd59c0e902977c01f9634f9
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-09 10:53:47 +0000

    Post endpoint created including its route, model, and controller.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyparser from 'body-parser';
+import routes from './routes/soccerRoutes';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,10 @@ mongoose.connect('mongodb://localhost/soccerDB', {
 
 // bodyparser setup
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+// register routes
+routes(app);
 
 app.get('/', (req, res) => 
   res.send(`Our soccer application is running on port ${PORT}.`)
